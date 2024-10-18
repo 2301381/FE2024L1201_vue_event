15055178887590f85b12aeb707f6aa21ed838126
2301381 <2301381@s.asojuku.ac.jp>
2024-10-18 10:20:59 +0900

    sample

--- counter/script/script.js
+++ counter/script/script.js
@@ -3,6 +3,26 @@ new Vue({
     el: '#app',
     data() {
         return {
+            count: 1,
+            number: 0,
         };
+    },
+    methods: {
+        increment() {
+            this.count = this.count * 2;
+        },
+        decrement() {
+            this.count = this.count / 2;
+        },
+        reset() {
+            this.count = 1;
+        },
+        tasu() {
+            this.number++;
+        },
+        hiku() {
+            this.number--;
+        }
+
     }
 });
